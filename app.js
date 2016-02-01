092a3ea93cea6929f4ac586787776ef7c1887d3b
Philip YS <wbvcos@gmail.com>
2016-02-02 01:27:59 +0900

    too many bugs

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var logger = require('morgan');
+
+var path = require('path');
+
+app.use(express.static(__dirname, '/public'));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+
+http.listen(3000, function(){
+	console.log('sex');
+})
