f1e429191362d48238bceff0ef4ab7ea5d4606ef
Philip YS <wbvcos@gmail.com>
2016-02-08 03:52:27 +0900

    add tab contents

---
+++ public/js/common.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$(".added_friend").hide();
+	$('.intersting').hide();
+});
+
+function move_tap(tab, content){
+	$(tab).click(function() {
+		$('.tap_content').hide();
+		$(content).show();
+		$('.tap').removeAttr('style');
+		$(this).css('color', 'white');
+	})
+};
+
+var taps = ['.history', '.friend', '.interested']
+var contents = ['.chat_history', '.added_friend', '.intersting'];
+
+
+for (var i = 0; i <= 3; i++) {
+	move_tap(taps[i], contents[i]);
+};
