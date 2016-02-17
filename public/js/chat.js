e996207069e48a468a456c5bacb00a84940a4fd2
Philip YS <wbvcos@gmail.com>
2016-02-17 21:28:38 +0900

    tidy up

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,40 +1,45 @@
 var socket = io();
 
-$('form').submit(function(){
-	var from = $('#user').val();
-	var msg = $('#input').val();
-	socket.emit('chat message', from, msg);
-	$('#input').val('');
+$('form').submit(function() {
+    var from = $('#user').val();
+    var msg = $('#input').val();
 
-	return false;
+    if (msg) {
+        socket.emit('chat message', from, msg);
+        $('#input').val('');
+    } else {
+        return false;
+    }
+    
+    return false;
 });
 
-socket.on('chat message', function(from, msg){
-	var me = $('#user').val();
-	var from = (from == me) ? 'You' : 'Someone';
-	send_msg(from, msg);
+socket.on('chat message', function(from, msg) {
+    var me = $('#user').val();
+    var from = (from == me) ? 'You' : 'Someone';
+    send_msg(from, msg);
 });
 
 
-function send_msg(from, msg){
-	var html = '<div class="msg_box"><img class="profile-pic" src="http://i.imgur.com/bNo8EHH.png"><div class="user_name_chat">{NAME}</div><div class="text">{MESSAGE}</div><div class="clear"></div>';
-	var append = html.replace('{NAME}', from);
-	html = append.replace('{MESSAGE}', msg);
-	console.log(html);
-	$('#messages').append(html);
+function send_msg(from, msg) {
+    var html = '<div class="msg_box"><img class="profile-pic" src="http://i.imgur.com/bNo8EHH.png"><div class="user_name_chat">{NAME}</div><div class="text">{MESSAGE}</div><div class="clear"></div>';
+    var append = html.replace('{NAME}', from);
+    html = append.replace('{MESSAGE}', msg);
+    console.log(html);
+    $('#messages').append(html);
 }
 
-$(document).ready(function(){
-  var name = makeid();
-  $('#user').val(name);
-  });
-  
+$(document).ready(function() {
+    var name = makeid();
+    $('#user').val(name);
+});
+
 function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  
-  for( var i=0; i < 5; i++ ) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 }
--- public/js/common.js
+++ public/js/common.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-	$(".added_friend").hide();
-	$('.intersting').hide();
+$(document).ready(function() {
+    $(".added_friend").hide();
+    $('.intersting').hide();
 });
 
-function move_tap(tab, content){
-	$(tab).click(function() {
-		$('.tap_content').hide();
-		$(content).show();
-		$('.tap').removeAttr('style');
-		$(this).css('color', 'white');
-	})
+function move_tap(tab, content) {
+    $(tab).click(function() {
+        $('.tap_content').hide();
+        $(content).show();
+        $('.tap').removeAttr('style');
+        $(this).css('color', 'white');
+    })
 };
 
 var taps = ['.history', '.friend', '.interested']
@@ -17,5 +17,5 @@ var contents = ['.chat_history', '.added_friend', '.intersting'];
 
 
 for (var i = 0; i <= 3; i++) {
-	move_tap(taps[i], contents[i]);
+    move_tap(taps[i], contents[i]);
 };
